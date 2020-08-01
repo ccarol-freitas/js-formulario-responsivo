b0f3338f045b71d3d59c4bae725005a7a40ae8f9
CAROLINE DE FREITAS PAULA <caroline.paula@infinity.local>
2020-08-01 00:22:53 -0300

    Update na validação dos campos

--- js/validacaoCampos.js
+++ js/validacaoCampos.js
@@ -1,5 +1,6 @@
 // Name: Carol Freitas
 // Date: 29/07/2020
+// Update: 31/07/2020
 // Description: lógica para validar os campos, para que eles não venham vazios
 
 
@@ -7,30 +8,24 @@ function validar() {
     var nome = formulario.nome.value;
     var idade = formulario.idade.value;
     var estado = formulario.estado.value;
-    var cidade = formulario.cidade.value;
 
     if (nome == "") {
-        console.log('linha 9')
-        alert('Por favor, preencha o campo nome :)');
-        formulario.nome.focus();
-        return false;
+        // console.log('linha 13')
+        document.getElementById("erronome").style.display = 'block';
+    } else {
+        document.getElementById("erronome").style.display = 'none';
     }
 
     if (idade == "") {
-        alert('Por favor, preencha o campo idade :)');
-        formulario.idade.focus();
-        return false;
+        document.getElementById("erroidade").style.display = 'block';
+    } else {
+        document.getElementById("erroidade").style.display = 'none';
     }
 
     if (estado == "") {
-        alert('Por favor, preencha o campo estado :)');
-        formulario.estadi.focus();
-        return false;
+        document.getElementById("erroestado").style.display = 'block';
+    } else {
+        document.getElementById("erroestado").style.display = 'none';
     }
 
-    if (cidade == "") {
-        alert('Por favor, preencha o campo cidade :)');
-        formulario.cidade.focus();
-        return false;
-    }
 }
